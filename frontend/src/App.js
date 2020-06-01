473b503c7179998eb6fa0566470488cb1249d209
Praveen Aravamudham <praveenaravamudham@Praveens-MBP.hsd1.ma.comcast.net>
2020-05-31 21:07:20 -0400

    added appjs again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
-import 'App.css';
+import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
